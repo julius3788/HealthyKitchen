b586dc0a9c6ef754bc6206426e47533975a80d4e
JULIUS SUTRISNO <julius3788@gmail.com>
2025-11-09 11:33:09 +0700

    change contact us text

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -38,8 +38,8 @@ const ContactUs = () => {
 
   return (
     <div
-        id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
-        <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
+        id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white  bg-gray-100 dark:bg-gray-950'>
+        <Title title='Reach out to us' desc='From our kitchen to your table, we prepare nutritious meals made with care — for a healthier you.' />
 
         <form           
         onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
